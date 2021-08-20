58198083c952ead75c58a2311379adff3f401644
k_bario <kylebario1@gmail.com>
2021-08-20 12:21:54 +0800

    bl_ corrected

--- R/bl_.R
+++ R/bl_.R
@@ -3,6 +3,7 @@
 #' @param x the spectrum to be baseline corrected
 #'
 #' @return an x with a corrected baseline
+#' @importFrom ptw asysm
 #' @export
 #'
 #' @examples
@@ -11,12 +12,6 @@ bl_ <- function(x){
   if (any(is.na(x))){
     x[is.na(x)]=0
   }
-  if (is.null(ncol(x))){
-    x <- t(x)
-    return(x)
-  }
-  xb <- t(apply(x, 1, function(i){
-    x <- x - ptw::asysm(x, maxit = 30, lambda = 1e+07)
-  }))
+  xb <- x-asysm(x, maxit = 30, lambda = 1e+07)
   return(xb)
 }
