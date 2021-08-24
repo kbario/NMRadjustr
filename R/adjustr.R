1626267e84a63aa744bb8e421d5f2a00c0dc235a
k_bario <kylebario1@gmail.com>
2021-08-24 14:23:38 +0800

    adjuster added args to pass to roundr

--- R/adjustr.R
+++ R/adjustr.R
@@ -4,11 +4,12 @@
 #' @param noi The noise
 #' @param goal The target signal to noise
 #' @param ns The number of scans used to create the provided signal and noise values
+#' @param r2 Arguments passed to embedded roundr function. See roundr help for details.
 #'
-#' @return The number of scans (rounded to the nearest multiple of 4 capped at 512) necessary to achieve the goal signal to noise
+#' @return The number of scans (rounded based on the input arguments from r2) necessary to achieve the goal signal to noise
 #' @export
 #'
 #' @examples
-adjustr <- function(sig, noi, goal, ns){
-  roundr(((goal/(sig/noi))^2)*ns)
+adjustr <- function(sig, noi, goal, ns, r2){
+  roundr(((goal/(sig/noi))^2)*ns, r2)
 }
